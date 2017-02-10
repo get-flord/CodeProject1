a63f17fe94194ec3d5a5d7f7d278c35bb2e999c7
get-flord <get.flord.jp@gmail.com>
2017-02-10 12:40:38 -0500

    Implements all but MurphyDuck (wya?)

--- headfirst/strategy/EdwardsDuck.java
+++ headfirst/strategy/EdwardsDuck.java
@@ -3,12 +3,12 @@ package headfirst.strategy;
 /**
  * Created by manahax on 2/10/17.
  */
-public class EdwardsDuck {
+public class EdwardsDuck extends Duck {
     public EdwardsDuck(){
         QuackBehavior q = new EdwardsQuack();
-        //flyBehavior = new FlyWithWings();
+        flyBehavior = new EdwardsFly();
     }
     public void display() {
-        System.out.println("卫生纸");}
-
+        System.out.println("卫生纸 - 'Toilet Paper'");
+    }
 }

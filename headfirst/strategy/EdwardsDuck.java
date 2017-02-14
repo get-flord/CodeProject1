f72509898095c59f77c5a14498d6da043149b92a
get-flord <get.flord.jp@gmail.com>
2017-02-14 09:46:13 -0500

    Added code for miniducksimulator, other fixes

--- headfirst/strategy/EdwardsDuck.java
+++ headfirst/strategy/EdwardsDuck.java
@@ -9,11 +9,7 @@ public class EdwardsDuck extends Duck {
         flyBehavior = new EdwardsFly();
     }
     public void display() {
-<<<<<<< HEAD
-        System.out.println("卫生纸 - 'Toilet Paper'");
+        System.out.println("I'm a real Edwards duck");
     }
-=======
-        System.out.println("I'm a real Edwards duck");}
 
->>>>>>> 0bb3c37ee526c249d67c313d32452b9e688fab38
 }
